2e4acb58d5698d9e834775f4ccafd10f5500b095
reyanson <79089809+reyanson@users.noreply.github.com>
2024-06-17 19:10:07 +0530

    #3, write the login endpoint code
    wrote the login endpoint code, store the username and password, and check the the stored username and user input username , generate the jwt and store into cookie, i create two type of jwt first one access token and second refresh token

--- server/index.js
+++ server/index.js
@@ -19,3 +19,38 @@ app.use(express.json())
 //parse cookies
 app.use(cookieParser())
 
+//Endpoint for user login
+app.post("/login",(req,res) =>{
+    const uname = "reyanson@gmail.com";
+    const pw = "Password@123"
+
+    const {username, password} = req.body;
+
+    //check if user is alredy logged in
+    const accessToken = req.cookies.accessToken;
+    console.log(accessToken)
+
+    if(!accessToken){
+        if(username === uname){
+            if(password === pw){
+                //generate the access token
+                const accessToken = jwt.sign({username:username},"jwt-access-token-secret-key", { expiresIn: '1m'});
+                const refreshToken = jwt.sign({username:username},"jwt-refresh-token-secret-key", { expiresIn: '5m'});
+
+                // set token as cookies in the response
+                res.cookie('accessToken', accessToken, { maxAge: 60000});
+                res.cookie('refreshToken', refreshToken, { maxAge: 300000, httpOnly:true, sameSite: 'strict'});
+
+                res.json("Login Sucessfull");
+            }else{
+                res.json("Incorrect Password");
+            }
+        }else{
+            res.json("No record existed. Please register first");
+        }
+    }else{
+        //user is already logged in , redirect to home page
+    }
+
+})
+
