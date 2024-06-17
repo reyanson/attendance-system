66fcba779a28cb04ad4ae3628e4eaffa23fa25f5
reyanson <79089809+reyanson@users.noreply.github.com>
2024-06-17 20:31:23 +0530

    #10, Redirect to the login page on logout

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -6,12 +6,59 @@ import 'sweetalert2/src/sweetalert2.scss'
 
 const Home = () => {
 
+    const [message, setMessage] = useState()
+    const navigate = useNavigate()
 
-return (
-    <div>
-        <h2>Home</h2>
-    </div>
-)
+    axios.defaults.withCredentials = true;
+    useEffect(() => {
+        axios.get("http://localhost:3001/home")
+            .then(res => {
+                if (res.data.valid) {
+                    setMessage(res.data.message)
+                } else {
+                    navigate("/")
+                }
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    const handleLogout = () => {
+        axios.post("http://localhost:3001/logout")
+            .then(() => {
+                let timerInterval;
+                Swal.fire({
+                title: "Logging out",
+                timer: 1000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading();
+                    const timer = Swal.getPopup().querySelector("b");
+                    timerInterval = setInterval(() => {
+                    timer.textContent = `${Swal.getTimerLeft()}`;
+                    }, 100);
+                },
+                willClose: () => {
+                    clearInterval(timerInterval);
+                }
+                }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    navigate("/")
+                }
+                });
+                
+            })
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <div>
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <h2>{message}</h2>
+                <button onClick={handleLogout}>Logout</button>
+            </div>
+        </div>
+    )
 }
 
 export default Home
