5dfdc7685cc38351b08bf19ac38605a42956b290
reyanson <79089809+reyanson@users.noreply.github.com>
2024-06-17 19:59:17 +0530

    #6, create the Home page

---
+++ client/src/Home.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
+
+const Home = () => {
+
+
+return (
+    <div>
+        <h2>Home</h2>
+    </div>
+)
+}
+
+export default Home
