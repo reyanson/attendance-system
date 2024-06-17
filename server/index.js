e756439264c57eb1eb60c42cea134843e1fce815
reyanson <79089809+reyanson@users.noreply.github.com>
2024-06-17 20:26:23 +0530

    #9, write the varifyuser , renewtoken methods and home , logout endpoint

--- server/index.js
+++ server/index.js
@@ -54,6 +54,59 @@ app.post("/login",(req,res) =>{
 
 });
 
+//Middleware to verify user's access token
+const varifyUser = (req,res, next) =>{
+    const accessToken = req.cookies.accessToken;
+    if (!accessToken) {
+        if (renewToken(req, res)) {
+            next();
+        }
+    } else {
+        jwt.verify(accessToken, 'jwt-access-token-secret-key', (err, decoded) => {
+            if (err) {
+                return res.json({ valid: false, message: "Invalid Token" });
+            } else {
+                req.email = decoded.email;
+                next();
+            }
+        });
+    }
+};
+
+// Function to renew the access token using the refresh token
+const renewToken = (req, res) => {
+    let exist = false;
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+        return res.json({ valid: false, message: "No Refresh Token" });
+    } else {
+        jwt.verify(refreshToken, 'jwt-refresh-token-secret-key', (err, decoded) => {
+            if (err) {
+                return res.json({ valid: false, message: "Invalid Refresh Token" });
+            } else {
+                const accessToken = jwt.sign({ email: decoded.email }, "jwt-access-token-secret-key", { expiresIn: '1m' });
+                res.cookie('accessToken', accessToken, { maxAge: 60000 });
+
+                exist = true;
+            }
+        });
+    }
+    return exist;
+};
+
+// Protected route that requires user authentication
+app.get('/home', varifyUser, (req, res) => {
+    return res.json({ valid: true, message: "Welcome to Home page " });
+});
+
+// Endpoint for user logout
+app.post("/logout", (req, res) => {
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+    res.json("Logged out successfully");
+});
+
+
 //start the express server
 app.listen(3001,() =>{
     console.log("Server is running on port 3001...")
